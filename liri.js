bbfe4f9fed89e408a38a9f5dd088114825094d78
Minh Pham <mqpham848@gmail.com>
2019-08-09 20:59:59 -0400

    pulled from api

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const axios = require('axios');
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 
@@ -8,25 +9,59 @@ let userSearch = process.argv[3];
 
 // console.log(userAction, userSearch);
 
+//switch statements used when looking at one variable (userAction) and gives 4 options
 switch (userAction) {
     case "concert-this":
-        console.log("concert-this");
+        findConcert();
         break;
 
     case "spotify-this-song":
-        console.log("spotify-this-song");
+        songSearch();
         break;
 
     case "movie-this":
-        console.log("movie-this");
+        movieSearch()
         break;
 
     case "do-what-it-says":
-        console.log("do-what-it-says");
+        justDoIt()
         break;
 
-    default: 
+    default:
         console.log("Input unfound");
         break;
 }
 
+function findConcert() {
+    console.log("concert-this", userSearch);
+
+    const queryURL = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp"
+
+    axios.get(queryURL)
+  .then(function (response) {
+    // handle success
+    let concertArray = response.data
+    // console.log(concertArray[0]);
+    console.log(concertArray[0].venue.name);
+    console.log(concertArray[0].venue.city);
+    // loop over concert array and console.log venue info for first five
+  })
+
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
+
+function songSearch() {
+    console.log("spotify-this-song", userSearch);
+
+}
+
+function movieSearch() {
+    console.log("movie-this", userSearch);
+}
+
+function justDoIt() {
+    console.log("do-what-it-says");
+}
